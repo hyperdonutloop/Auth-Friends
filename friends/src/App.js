d91581051a08c694eb43d53d81c906c84e295ec4
donutwizard666 <thebeardedwarrior@icloud.com>
2019-11-12 14:34:37 -0800

    built out private route component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
 import './App.css';
 import FriendsForm from './components/FriendsForm';
+import PrivateRoute from './utils/PrivateRoute';
 
 function App() {
   return (
-    <div className="App">
-     <FriendsForm />
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+          <li>
+            <Link to='/friends'>My Friends</Link>
+          </li>
+        </ul>
+        <Switch>
+          <PrivateRoute path='/friends'>
+            <FriendsList />
+          </PrivateRoute>
+          <FriendsForm />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
