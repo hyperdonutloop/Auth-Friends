560437362bb03be3302d60700d71e8cf4a1c7292
donutwizard666 <thebeardedwarrior@icloud.com>
2019-11-12 22:34:55 -0800

    fixed some issues on the friends form component

--- friends/src/components/FriendsForm.jsx
+++ friends/src/components/FriendsForm.jsx
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const FriendsForm = () => {
-  
+const FriendsForm = ({ setFriends }) => {
+  const [ friend, setFriend ] = useState({
+    name: '',
+    age: '',
+    email: ''
+  });
+
+  const handleChanges = event => {
+    setFriend({ ...friend, [event.target.name]: event.target.value });
+  };
+
+  const addFriend = (e, obj) => {
+    e.preventDefault();
+    const token = localStorage.getItem('token');
+    if (obj.name && obj.email && obj.age) {
+      axiosWithAuth()
+        .post('http://localhost:5000/api/friends', obj, token)
+        .then(response => {
+          setFriends(response.data);
+
+        })
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={e => addFriend(e, friend)} action=''>
       <label>Add Some Friends</label>
       <input
         type="text"
         name="name" 
         placeholder="name"
+        value={friend.name}
+        onChange={handleChanges}
       />
       <input
         type="text"
         age="age" 
         placeholder="age"
+        value={friend.age}
+        onChange={handleChanges}
       />
       <input
         type="text"
         email="email" 
         placeholder="email"
+        value={friend.email}
+        onChange={handleChanges}
       />
-      <button>Add Friend</button>
+      <button>Add Friend!</button>
     </form>
   )
 }
--- friends/src/components/FriendsList.jsx
+++ friends/src/components/FriendsList.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import FriendsForm from './FriendsForm';
+
 
 const FriendsList = () => {
+  const [ friends, setFriends ] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axiosWithAuth()
+      .get('http://localhost:5000/api/friends')
+      .then(response => {
+        setFriends(response.data);
+      })
+      .catch(error => {
+        console.log('the data was not returned', error);
+        
+      })
+  }
+
   return (
     <div>
-
+      <h1>These Are My Friends</h1>
+      <FriendsForm setFriends={setFriends}/>
+      {friends.map(item => (
+        <Friend key={item.id} setFriends={setFriends} friend={item} />
+      ))}
     </div>
   )
 }
