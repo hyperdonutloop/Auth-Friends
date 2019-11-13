6fb4ea665f537d12de0150a39923bb871a5cc7f4
donutwizard666 <thebeardedwarrior@icloud.com>
2019-11-13 09:48:44 -0800

    added comments in axios with auth component

--- friends/src/utils/axiosWithAuth.jsx
+++ friends/src/utils/axiosWithAuth.jsx
@@ -10,3 +10,13 @@ export const axiosWithAuth = () => {
   });
 };
 
+/*
+1.create file called axiosWithAuth 
+2. import axios
+3. set up function
+4. inside the function we are using the getItem method. The getItem method allows you to access the data stored in the browsers local storage object. It only accepts one parameter, which is the KEY and returns the value as a STRING.
+5. TOKENS are issued by the server and they are strings of random characters. A request has to have a header and a property of token
+6.The token will be returned to us by the server after the user properly authenticates (properly logs in 😁). When you login, you want to save the token that’s returned to localStorage, so that the above axiosWithAuth module can grab it for other calls that require the Authorization header to be on it.
+*/
+
+
