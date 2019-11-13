09157898ce5c388cc3791ea5429dc4688a4ba496
donutwizard666 <thebeardedwarrior@icloud.com>
2019-11-12 18:29:04 -0800

    was able to get routes and protected routes set up correctly

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import './App.css';
-import FriendsForm from './components/FriendsForm';
+// import FriendsForm from './components/FriendsForm';
 import PrivateRoute from './utils/PrivateRoute';
-import FriendsList from './components/FriendsList';
 import Login from './components/Login';
+import FriendsList from './components/FriendsList';
 
 function App() {
   return (
@@ -20,10 +20,7 @@ function App() {
           </li>
         </ul>
         <Switch>
-          <PrivateRoute path='/friends'>
-            <FriendsList />
-            <FriendsForm />
-          </PrivateRoute>
+          <PrivateRoute exact path='/friends' component={FriendsList} />
           <Route path='/login' component={Login} />
           
         </Switch>
